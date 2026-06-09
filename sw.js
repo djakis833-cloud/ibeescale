0b5e730bb73871261ff80470ab72c8e8aa0103d9
djakis833-cloud <djakis833@gmail.com>
2026-06-09 12:52:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-const CACHE = 'zigaria-v2';
+const CACHE = 'zigaria-v6';
 const ASSETS = [
   './index.html',
   './manifest.json',
